eef9b8f393b3e5fa0f87b50f628b2b0f78f45415
Adam Faulconbridge <afaulconbridge@googlemail.com>
2019-05-07 21:57:30 +0100

    Updates to 0.17.35 compatibility

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 Are you tired of building a starter base to research bots only to tear it down while waiting too long for bots to recharge? Uber Bot Start lets you start with construction bots in a Vanilla+ way. You'll start with:
 
 * Power armor Mk.2
-* 50 construction robots
+* 100 construction robots
 * 4 personal roboport Mk.2
 * 4 portable fusion reactors
-* 2 battery Mk.2
-* 4 Energy Shield Mk.2
+* 4 battery Mk.2
+* Exoskeleton
+* Night vision goggles
 * 1000 iron plates
 * 500 copper plates
 
--- control.lua
+++ control.lua
@@ -4,25 +4,25 @@ script.on_event(defines.events.on_player_created, function(event)
 
 	items = {
 		{"power-armor-mk2", 1},
-		{"construction-robot", 50},
+		{"construction-robot", 100},
 		{"iron-plate", 1000},
 		{"copper-plate", 500},
 	}
 	armor = {
 		{"fusion-reactor-equipment"},
 		{"fusion-reactor-equipment"},
-		{"personal-roboport-mk2-equipment"},
-		{"personal-roboport-mk2-equipment"},
 		{"fusion-reactor-equipment"},
 		{"fusion-reactor-equipment"},
+		{"exoskeleton-equipment"},
+		{"personal-roboport-mk2-equipment"},
+		{"personal-roboport-mk2-equipment"},
 		{"personal-roboport-mk2-equipment"},
 		{"personal-roboport-mk2-equipment"},
 		{"battery-mk2-equipment"},
 		{"battery-mk2-equipment"},
-		{"energy-shield-mk2-equipment"},
-		{"energy-shield-mk2-equipment"},
-		{"energy-shield-mk2-equipment"},
-		{"energy-shield-mk2-equipment"},
+		{"battery-mk2-equipment"},
+		{"battery-mk2-equipment"},
+		{"night-vision-equipment"},
 	}
 
 	local player = game.players[event.player_index]
@@ -30,7 +30,7 @@ script.on_event(defines.events.on_player_created, function(event)
 		player.insert{name = v[1], count = v[2]}
 	end
 
-	local grid = player.get_inventory(defines.inventory.player_armor)[1].grid
+	local grid = player.get_inventory(defines.inventory.character_armor)[1].grid
 	for  i, v in pairs(armor) do
 		grid.put({name = v[1]})
 	end
