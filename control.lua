ea3d2dbc6a40d0fd9daac2d00137bc483d3c2600
Adam Faulconbridge <afaulconbridge@googlemail.com>
2021-02-05 09:29:05 +0000

    Fixing

--- control.lua
+++ control.lua
@@ -1,38 +1,40 @@
-script.on_event(defines.events.on_player_created, function(event)
-	local items
-	local armor
+local function AddBotsToInventory(e)
+	local player = game.players[e.player_index]	
+	
+	if not player.get_main_inventory() then return end
+	
+	local inventory = player.get_inventory(defines.inventory.character_armor)
+	inventory.insert{name="power-armor-mk2", count = 1}
+	
+	local suit = player.get_inventory(defines.inventory.character_armor)[1].grid
+	suit.put({name = "fusion-reactor-equipment"})
+	suit.put({name = "fusion-reactor-equipment"})
+	suit.put({name = "fusion-reactor-equipment"})
+	suit.put({name = "fusion-reactor-equipment"})
+	suit.put({name = "exoskeleton-equipment"})
+	suit.put({name = "personal-roboport-mk2-equipment"})
+	suit.put({name = "personal-roboport-mk2-equipment"})
+	suit.put({name = "personal-roboport-mk2-equipment"})
+	suit.put({name = "personal-roboport-mk2-equipment"})
+	suit.put({name = "battery-mk2-equipment"})
+	suit.put({name = "battery-mk2-equipment"})
+	suit.put({name = "battery-mk2-equipment"})
+	suit.put({name = "battery-mk2-equipment"})
+	suit.put({name = "night-vision-equipment"})
+	player.insert{name="construction-robot", count = 100}
+	player.insert{name="iron-plate", count = 1000}
+	player.insert{name="copper-plate", count = 500}
+end
 
-	items = {
-		{"power-armor-mk2", 1},
-		{"construction-robot", 100},
-		{"iron-plate", 1000},
-		{"copper-plate", 500},
-	}
-	armor = {
-		{"fusion-reactor-equipment"},
-		{"fusion-reactor-equipment"},
-		{"fusion-reactor-equipment"},
-		{"fusion-reactor-equipment"},
-		{"exoskeleton-equipment"},
-		{"personal-roboport-mk2-equipment"},
-		{"personal-roboport-mk2-equipment"},
-		{"personal-roboport-mk2-equipment"},
-		{"personal-roboport-mk2-equipment"},
-		{"battery-mk2-equipment"},
-		{"battery-mk2-equipment"},
-		{"battery-mk2-equipment"},
-		{"battery-mk2-equipment"},
-		{"night-vision-equipment"},
-	}
+local function player_created(e)
+  AddBotsToInventory(e)
+end
 
-	local player = game.players[event.player_index]
-	for i, v in pairs(items) do
-		player.insert{name = v[1], count = v[2]}
-	end
+local function cutscene_cancelled(e)
+  if remote.interfaces["freeplay"] then   -- Added for 1.0 freeplay cutscene
+    AddBotsToInventory(e)
+  end
+end
 
-	local grid = player.get_inventory(defines.inventory.character_armor)[1].grid
-	for  i, v in pairs(armor) do
-		grid.put({name = v[1]})
-	end
-
-end)
+script.on_event(defines.events.on_player_created, player_created)
+script.on_event(defines.events.on_cutscene_cancelled, cutscene_cancelled)
